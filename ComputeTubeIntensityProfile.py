2202eb00731c075768bc5abe9e0a3bbd3c45d151
Deepak Roy Chittajallu <Deepak.Chittajallu@kitware.com>
2014-12-31 18:16:30 -0500

    Halfway through making my ComputeTubeIntensityProfile CLI utility to act as a slicer module

--- ComputeTubeIntensityProfile.py
+++ ComputeTubeIntensityProfile.py
@@ -2,10 +2,192 @@
 
 import numpy as np
 import SimpleITK as sitk
-import scipy.ndimage
+
+#
+# ComputeTubeIntensityProfile
+#
+
+class ComputeTubeIntensityProfile:
+    def __init__( self, parent ):
+        parent.title = "ComputeTubeIntensityProfile"
+        parent.categories = ["Informatics"]
+        parent.dependencies = []
+        parent.contributors = ["Deepak Roy Chittajallu (Kitware)"]
+        parent.helpText = """
+        Given a tube's fly through image and mask data, computes the intensity 
+        profiles along a cross-section perpendicular to the tube's tangent at 
+        each point on the tube's center line, and generates a plot depicting the
+        median, lower and upper quartiles of the extracted intesnity profiles.
+        """
+        self.parent = parent
+        
+#
+# qLComputeTubeIntensityProfileWidget
+#
+
+class ComputeTubeIntensityProfileWidget:
+    def __init__( self, parent = None ):
+        
+        #from __main__ import vtk, qt, ctk, slicer
+        self.slicer = __import__("slicer", fromlist="__main__")
+        self.qt = __import__("qt", fromlist="__main__")
+        self.ctk = __import__("ctk", fromlist="__main__")
+        
+        if not parent:
+            self.parent = self.slicer.qMRMLWidget()
+            self.parent.setLayout(self.qt.QVBoxLayout())
+            self.parent.setMRMLScene(self.slicer.mrmlScene)
+        else:
+            self.parent = parent
+
+        self.layout = self.parent.layout()
+        if not parent:
+            self.setup()
+            self.parent.show()
+            
+        self.isDeveloperMode = self.qt.QSettings().value('QtTesting/Enabled')
+                
+    def setup(self):
+        # Instantiate and connect widgets ...
+
+        #
+        # Reload and Test area
+        #  
+        if self.isDeveloperMode:
+            
+            reloadCollapsibleButton = self.ctk.ctkCollapsibleButton()
+            reloadCollapsibleButton.text = "Reload && Test"
+            reloadCollapsibleButton.collapsed = True
+            self.layout.addWidget(reloadCollapsibleButton)
+            reloadFormLayout = self.qt.QFormLayout(reloadCollapsibleButton)
+            
+            self.reloadButton = self.qt.QPushButton("Reload")
+            self.reloadButton.toolTip = "Reload this module."
+            reloadFormLayout.addWidget(self.reloadButton)
+            self.reloadButton.connect('clicked()', self.onReload)
+
+        #
+        # I/O
+        #
+        ioCollapsibleButton = self.ctk.ctkCollapsibleButton()
+        ioCollapsibleButton.text = "Input/Output"
+        ioCollapsibleButton.collapsed = False
+        self.layout.addWidget(ioCollapsibleButton)
+        
+        # Layout within the parameters collapsible button
+        ioFormLayout = self.qt.QFormLayout(ioCollapsibleButton)
+        
+        # input tube fly through image selector
+        self.inTubeImageSelector = self.slicer.qMRMLNodeComboBox()
+        self.inTubeImageSelector.nodeTypes = ( ("vtkMRMLScalarVolumeNode"), 
+                                             "" )
+        self.inTubeImageSelector.addAttribute( "vtkMRMLScalarVolumeNode", 
+                                             "LabelMap", 0 )
+        self.inTubeImageSelector.selectNodeUponCreation = True
+        self.inTubeImageSelector.addEnabled = False
+        self.inTubeImageSelector.removeEnabled = False
+        self.inTubeImageSelector.noneEnabled = False
+        self.inTubeImageSelector.showHidden = False
+        self.inTubeImageSelector.showChildNodeTypes = False
+        self.inTubeImageSelector.setMRMLScene( self.slicer.mrmlScene )
+        self.inTubeImageSelector.setToolTip( "Pick the tube fly through \
+                                              image." )
+        ioFormLayout.addRow( "Input Tube Fly-through Image: ", 
+                                self.inTubeImageSelector)
+        
+
+        # input tube fly through mask image selector
+        self.inTubeMaskSelector = self.slicer.qMRMLNodeComboBox()
+        self.inTubeMaskSelector.nodeTypes = ( ("vtkMRMLScalarVolumeNode"), 
+                                                "" )
+        self.inTubeMaskSelector.addAttribute( "vtkMRMLScalarVolumeNode", 
+                                               "LabelMap", 1 )
+        self.inTubeMaskSelector.selectNodeUponCreation = True
+        self.inTubeMaskSelector.addEnabled = False
+        self.inTubeMaskSelector.removeEnabled = False
+        self.inTubeMaskSelector.noneEnabled = False
+        self.inTubeMaskSelector.showHidden = False
+        self.inTubeMaskSelector.showChildNodeTypes = False
+        self.inTubeMaskSelector.setMRMLScene( self.slicer.mrmlScene )
+        self.inTubeMaskSelector.setToolTip( "Pick the tube fly through \
+                                             mask image." )
+        ioFormLayout.addRow( "Input Tube Fly-through Mask: ", 
+                                self.inTubeMaskSelector)
+        
+        #
+        # output tube intensity profile array selector
+        #
+        self.outTubeProfileSelector = self.slicer.qMRMLNodeComboBox()
+        self.outTubeProfileSelector.nodeTypes = ( ("vtkMRMLDoubleArrayNode"), 
+                                                  "" )
+        self.outTubeProfileSelector.addEnabled = True
+        self.outTubeProfileSelector.removeEnabled = True
+        self.outTubeProfileSelector.noneEnabled = False
+        self.outTubeProfileSelector.showHidden = False
+        self.outTubeProfileSelector.showChildNodeTypes = False
+        self.outTubeProfileSelector.setMRMLScene( self.slicer.mrmlScene )
+        self.outTubeProfileSelector.setToolTip( "Pick the output tube \
+                                                 intensity profile array." )
+        ioFormLayout.addRow("Output Tube Intensity Profile: ", 
+                            self.outTubeProfileSelector)
+
+        #
+        # Apply Button
+        #
+        self.applyButton = self.qt.QPushButton("Apply")
+        self.applyButton.toolTip = "Run the algorithm."
+        self.applyButton.enabled = False
+        ioFormLayout.addRow(self.applyButton)
+        self.onSelect()
+    
+        # connections
+        self.applyButton.connect('clicked(bool)', self.onApplyButton)
+        self.inTubeImageSelector.connect( "currentNodeChanged(vtkMRMLNode*)", 
+                                         self.onSelect )
+        self.inTubeMaskSelector.connect( "currentNodeChanged(vtkMRMLNode*)", 
+                                         self.onSelect )
+        self.outTubeProfileSelector.connect("currentNodeChanged(vtkMRMLNode*)", 
+                                            self.onSelect)
+    
+        # Add vertical spacer
+        self.layout.addStretch(1)
+
+    def onSelect(self):
+        self.applyButton.enabled = self.inTubeImageSelector.currentNode() and \
+                                   self.inTubeMaskSelector.currentNode() and \
+                                   self.outTubeProfileSelector.currentNode()
+
+    def onApplyButton(self):
+        logic = ComputeTubeIntensityProfileLogic()
+        logic.run(self.inTubeImageSelector.currentNode(), 
+                  self.inTubeMaskSelector.currentNode(),
+                  self.outTubeProfileSelector.currentNode())
+
+    def onReload(self,moduleName="ComputeTubeIntensityProfile"):
+        """Generic reload method for any scripted module.
+        ModuleWizard will subsitute correct default moduleName.
+        """
+        globals()[moduleName] = self.slicer.util.reloadScriptedModule(moduleName)
+
+#
+# ComputeTubeIntensityProfileLogic
+#
+
+class ComputeTubeIntensityProfileLogic:
+    """
+    This class should implement all the actual 
+    computation done by your module.  The interface 
+    should be such that other python code can import
+    this class and make use of the functionality without
+    requiring an instance of the Widget
+    """
+    def __init__(self):
+        self.chartNodeID = None
+        pass
+    def run(self, inTubeImage, inTubeMask, outTubeProfile):
+        
 
 def ComputeTubeIntensityProfileStats(intensityProfiles):
-    
     profileMedian = np.percentile( intensityProfiles, 50, 0 )
     profileLowerQuartile = np.percentile( intensityProfiles, 25, 0 )
     profileUpperQuartile = np.percentile( intensityProfiles, 75, 0 )        
@@ -13,30 +195,29 @@ def ComputeTubeIntensityProfileStats(intensityProfiles):
     return (profileMedian, profileLowerQuartile, profileUpperQuartile)
     
     
-def ComputeTubeIntensityProfiles(tubeFlyThroughImageFile, 
-                                 tubeFlyThroughMaskFile):
+def ComputeTubeIntensityProfiles(imTubeSitk, 
+                                 imTubeMaskSitk):
     """
-    Given a tube's fly through image and mask files, this function computes
-    the intensity profiles along a cross-section perpendicular to the tube's 
-    tangent at each point on the tube's center line
+    Given a tube's fly through image and mask as sitk image objects, this 
+    function computes the intensity profiles along a cross-section 
+    perpendicular to the tube's tangent at each point on the tube's 
+    center line
     
     Usage:
     
     (intensityProfiles, 
-     distToTubeCenter) = ComputeTubeIntensityProfiles(tubeFlyThroughImageFile, 
-                                                      tubeFlyThroughMaskFile)
+     distToTubeCenter) = ComputeTubeIntensityProfiles(imTubeSitk, 
+                                                      imTubeMaskSitk)
     Input:
 
-        tubeFlyThroughImageFile:  
+        imTubeSitk:  
         
-            Path to an image file containing the fly through 
-            image data of a tube.
+            Tube fly through image as an sitk object
         
         tubeFlyThroughMaskFile:   
         
-            Path to an image file where in the non-zero pixels
-            denote the pixels inside the tube in the tube's fly
-            through image file.
+            Tube fly through mask as an sitk object where in non-zero pixels
+            denote the pixels inside the tube.
     
     Output:
     
@@ -53,15 +234,14 @@ def ComputeTubeIntensityProfiles(tubeFlyThroughImageFile,
             in the intensityProfiles matrix        
     """
 
-    # load tube image     
-    imTubeSitk = sitk.ReadImage( tubeFlyThroughImageFile )
+    import scipy.ndimage
+    
+    # Grab input
     imTube = sitk.GetArrayFromImage( imTubeSitk )
 
     imageSize = imTubeSitk.GetSize()
     imageSpacing = imTubeSitk.GetSpacing()        
     
-    # load tube mask
-    imTubeMaskSitk = sitk.ReadImage( tubeFlyThroughMaskFile )
     imTubeMask = sitk.GetArrayFromImage( imTubeMaskSitk )
 
     # Extract mid-y aka coronal cross section    
@@ -115,7 +295,6 @@ def ComputeTubeIntensityProfiles(tubeFlyThroughImageFile,
         
 def main():    
     
-    import os
     import argparse
     import matplotlib.pyplot as plt
     
@@ -162,16 +341,22 @@ def main():
                              
     args = inputParser.parse_args()
 
+    # read tube fly through image file
+    imTubeSitk = sitk.ReadImage( args.tubeFlyThroughImageFile )
+
+    # read tube mask file    
+    imTubeMaskSitk = sitk.ReadImage( args.tubeFlyThroughMaskFile )
+    
     # compute tube intensity profiles    
     (tubeIntensityProfiles, 
-     distToTubeCenter) = ComputeTubeIntensityProfiles(
-                                     args.tubeFlyThroughImageFile,
-                                     args.tubeFlyThroughMaskFile )
+     distToTubeCenter) = ComputeTubeIntensityProfiles( imTubeSitk,
+                                                       imTubeMaskSitk )
                                      
     # compute profile stats                                         
-    profileMedian = np.percentile( tubeIntensityProfiles, 50, 0 )
-    profileLowerQuartile = np.percentile( tubeIntensityProfiles, 25, 0 )
-    profileUpperQuartile = np.percentile( tubeIntensityProfiles, 75, 0 )   
+    (profileMedian, 
+     profileLowerQuartile, 
+     profileUpperQuartile) = ComputeTubeIntensityProfileStats( 
+                                                 tubeIntensityProfiles )                                    
 
     # display
     plt.plot(distToTubeCenter, profileMedian, linewidth=2.0)
